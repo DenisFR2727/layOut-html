1618b648191dd1288f9bae45760ca0e0122ed2cc
Denys <dh92fr@ukr.net>
2025-07-10 12:49:34 +0300

    add fotos_h2 and style

--- JimmyFairly/src/script/script.js
+++ JimmyFairly/src/script/script.js
@@ -1,22 +1,4 @@
 // Slider Header
-// const headerMain = document.querySelector('.header__main');
-
-// const images = [
-//     './images/header/header-bg.png',
-//     './images/header/header-bg2.png',
-// ];
-
-// let currentIndex = 0;
-
-// function changeBackground() {
-//     currentIndex = (currentIndex + 1) % images.length;
-//     setTimeout(() => {
-//         headerMain.style.backgroundImage = `url(${images[currentIndex]})`;
-//         headerMain.style.transition = 'opacity 1s ease-in-out';
-//     }, 3000);
-// }
-
-// setInterval(changeBackground, 3000);
 import Splide from '@splidejs/splide';
 
 new Splide('.splide').mount();
